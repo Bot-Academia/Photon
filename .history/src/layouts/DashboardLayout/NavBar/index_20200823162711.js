8045ed3c333233580999a3077c72fc6e28259761
Hrishikesh Agarwal <hrishikeshagarwalv@gmail.com>
2020-08-23 23:42:01 +0530

    changed name

--- .history/src/layouts/DashboardLayout/NavBar/index_20200823162711.js
+++ .history/src/layouts/DashboardLayout/NavBar/index_20200823162711.js
@@ -27,7 +27,7 @@ import NavItem from './NavItem';
 const user = {
   avatar: '/static/images/avatars/avatar_6.png',
   jobTitle: 'Student Developer',
-  name: 'Gitty'
+  name: 'Vishally Chauhan'
 };
 
 const items = [
